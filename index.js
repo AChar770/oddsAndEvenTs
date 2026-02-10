535143b3334f08e058a350fd194a564d3104e571
AChar770 <amandatcharles@yahoo.com>
2026-02-10 16:39:27 -0500

    sort the first number function

--- index.js
+++ index.js
@@ -9,3 +9,15 @@ function addNumber(num) {
     state.bank.push(num);
     render();
 }
+function sortOne() {
+  if (state.bank.length === 0) return;
+  const number = state.bank.shift();
+  if (number % 2 === 0) {
+    state.evens.push(number);
+  } else {
+    state.odds.push(number);
+  }
+  
+  render();
+}
+
